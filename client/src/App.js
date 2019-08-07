63f10699c1521a3e7a500f640cb9047dcab6b344
Adam Postma <postama29@gmail.com>
2019-08-06 19:51:12 -0600

    route to inmates detail page (incomplete)

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,19 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Inmates} />
-          <Route exact path="/inmates" component={Inmates} />
-          {/* <Route exact path="/inmate/:id" component={Inmate} /> */}
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Inmates} />
+                    <Route exact path="/inmates" component={Inmates} />
+                    <Route exact path="/inmates/:id" component={Detail} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+import InmateService from "../utils/InmateService";
 
 class Detail extends Component {
   state = {
@@ -11,7 +11,7 @@ class Detail extends Component {
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
-    API.getBook(this.props.match.params.id)
+    InmateService.getInmate(this.props.match.params.id)
       .then(res => this.setState({ book: res.data }))
       .catch(err => console.log(err));
   }
